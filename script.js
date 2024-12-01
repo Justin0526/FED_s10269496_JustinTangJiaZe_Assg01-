41e7f48308bae7fe9e15843d0e633790115b285e
Justin Tang <jtang8446@gmail.com>
2024-12-01 18:55:01 +0800

    Finalise changes v1, subtracting additional spaces and last version of renaming classes and id

--- script.js
+++ script.js
@@ -1,68 +1,89 @@
-
-document.addEventListener("DOMContentLoaded", function(){
-  if (document.body.id === "programPage"){
-    return;
-  }
-  const ProgramURL = document.querySelectorAll(".programLink");
-
-  ProgramURL.forEach(function(programLink){
-    programLink.setAttribute("href","http://127.0.0.1:5501/ProgramPage.html");
-  })
-})
-
+// -------------------- Not Home Page -------------------- //
 document.addEventListener("DOMContentLoaded", function() {
+  // Check if the body has an id of 'homePage'
   if (document.body.id !== "homePage") {
     console.log("Slideshow script skipped for this page.");
-    
+
+    // Select all elements with the class 'HomeLink'
     const HomeURL = document.querySelectorAll(".HomeLink");
 
     HomeURL.forEach(function(homeLink){
+      // Set the href attribute of each 'HomeLink' to the specified Home URL
       homeLink.setAttribute("href", "http://127.0.0.1:5501/index.html");
     })
     return;
   }
   
-  // Slideshow logic here...
-  let slideIndex = 1;
-  showSlides(slideIndex);
+  // ----- Slideshow ----- //
+  // Initialize the slide index to 1 (first slide)
+  let slideNum = 1;
 
+  // Call the showSlides function to display the first slide
+  showSlides(slideNum);
+
+  // Function to change slides by a given number 'n' (next or previous)
   window.plusSlides = function(n) {
-    showSlides(slideIndex += n);
+    showSlides(slideNum += n);
   }
-
+  
+  // Function to set the current slide based on a specific index 'n'
   window.currentSlide = function(n) {
-    showSlides(slideIndex = n);
+    showSlides(slideNum = n);
   }
 
+  // Function to display the slide corresponding to the current 'slideNum'
   function showSlides(n) {
-    let i;
+    let num;
+
+    // Get all elements with the class 'mySlides' (all slides)
     let slides = document.getElementsByClassName("mySlides");
+
+    // Get all elements with the class 'dot' (navigation dots)
     let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}    
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+
+    // If the slide index is greater than the number of slides, reset it to 1
+    if (n > slides.length) {
+      slideNum = 1;
+    } 
+    // If the slide index is less than 1, set it to the last slide   
+    if (n < 1) {
+      slideNum = slides.length;
     }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+
+    // Hide all slides by setting their display to "none"
+    for (num = 0; num < slides.length; num++) {
+      slides[num].style.display = "none";  
     }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
+
+    // Remove the "active" class from all dots (used for navigation highlight)
+    for (num = 0; num < dots.length; num++) {
+      dots[num].className = dots[num].className.replace(" active", "");
+    }
+
+    // Display the current slide by setting its display to "block"
+    slides[slideNum-1].style.display = "block";  
+
+    // Add the "active" class to the dot corresponding to the current slide
+    dots[slideNum-1].className += " active";
   };
   
 });
+
+// -------------------- Is LTSkidsPage -------------------- //
 document.addEventListener("DOMContentLoaded", function(){
   if (document.body.id !== "LTSkidsPage"){
+    // Check if the body has an id of 'LTSkidsPage'
     console.log("No registration form here!");
     const KidsURL = document.querySelectorAll(".child");
 
+    // Select all elements with the class 'child'
     KidsURL.forEach(function(kidsLink){
+      // Set the href attribute of each 'child' to the specified Kids URL
       kidsLink.setAttribute("href", "http://127.0.0.1:5501/LTSkids.html");
     })
-
     return;
   }
-  function scrollToForm(){
+  function GoToRegister(){
     //Get the element with the id 'signupForm'
     const formSection = document.getElementById('SignUpForms');
 
@@ -74,54 +95,84 @@ document.addEventListener("DOMContentLoaded", function(){
   const registerButton = document.querySelector(".register");
 
   if (registerButton){
-    registerButton.addEventListener("click", scrollToForm);
+    registerButton.addEventListener("click", GoToRegister);
   }
 })
 
+// -------------------- Not Program Page -------------------- //
 document.addEventListener("DOMContentLoaded", function(){
+  // Check if the body has an id of 'programPage'
+  if (document.body.id === "programPage"){
+    return;
+  }
+  // Select all elements with the class 'programLink'
+  const ProgramURL = document.querySelectorAll(".programLink");
+
+  // Iterate through each 'programLink' element
+  ProgramURL.forEach(function(programLink){
+    // Set the href attribute of each 'programLink' to the specified ProgramPage URL
+    programLink.setAttribute("href","http://127.0.0.1:5501/ProgramPage.html");
+  })
+})
+
+// -------------------- Submit successful message for Home Page and LTSkidsPage -------------------- //
+// Wait until the DOM content is fully loaded
+document.addEventListener("DOMContentLoaded", function(){
+  // Check if the body has an id of 'homePage' and 'LTSkidsPage', otherwise exit
   if (document.body.id !== "homePage" && document.body.id !== "LTSkidsPage"){
     console.log("No form in this page");
     return;
   }
+
+  // Determine which form to select based on the body id
   const formId = (document.body.id === "homePage") ? "contactForm" : "RegistrationForm";
-    const form = document.getElementById(formId);
-    const modal = document.getElementById('successModal');
-    const closeButton = document.querySelector('.close-button');
 
-    // Handle form submission
-    form.addEventListener('submit', function (event) {
-        event.preventDefault();  // Prevent form from submitting normally
-        modal.style.display = 'flex';  // Show the success modal
-        this.reset();  // Optionally reset the form
-    });
+  // Get the form element by its ID
+  const form = document.getElementById(formId);
 
-    // Close the modal when the close button is clicked
-    closeButton.addEventListener('click', function () {
-        modal.style.display = 'none';
-    });
+  // Get the modal and the close button elements
+  const modal = document.getElementById('successModal');
+  const closeButton = document.querySelector('.close-button');
 
-    // Close the modal if the user clicks outside the modal content
-    window.addEventListener('click', function (event) {
-        if (event.target === modal) {
-            modal.style.display = 'none';
-        }
-    });
+  // Add an event listener to handle form submission
+  form.addEventListener('submit', function (event) {
+      event.preventDefault();  // Prevent form from submitting normally
+      modal.style.display = 'flex';  // Show the success modal
+      this.reset();  // reset the form
+  });
+
+  // Add an event listener to close the modal when the close button is clicked
+  closeButton.addEventListener('click', function () {
+      modal.style.display = 'none';  // Hide the modal when the close button is clicked
+  });
+
+  // Close the modal if the user clicks outside the modal content
+  window.addEventListener('click', function (event) {
+      if (event.target === modal) {
+          modal.style.display = 'none';
+      }
+  });
 })
 
-
-let progress = document.getElementById('progressbar');
-let totalHeight = document.body.scrollHeight - window.innerHeight;
-
-let mybutton = document.getElementById("myBtn");
-
+// ---- Scroll Progress bar ---- // 
+// When the user scrolls, call both the scroll function and update the progress bar
 window.onscroll = function() {
   scrollFunction();
   updateProgressBar();};
 
-// Back to Top Button 
+// Get the progress bar element
+let progress = document.getElementById('progressbar');
+
+// Calculate the total scrollable height (entire document height - visible window height)
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+
+// Get the 'Back to Top' button element
+let mybutton = document.getElementById("BckToTopButn");
+
+// ---- Back to Top Button ---- //
 function scrollFunction() {
-  // When the user scrolls down 20px from the top of the document, show the button
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  // When the user scrolls down 30px from the top of the document, show the button
+  if ( document.documentElement.scrollTop > 30 || document.body.scrollTop > 30) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
@@ -136,10 +187,12 @@ function topFunction() {
 
 //progress bar
 function updateProgressBar(){
+   // Calculate the scroll percentage based on the current scroll position
   let progressHeight = (window.pageYOffset / totalHeight) * 100;
+
+  // Set the height of the progress bar based on the calculated percentage
   progress.style.height = progressHeight + "%"
 }
-
 
 // Get elements
 let searchIcon = document.getElementById('searchIcon');
@@ -149,26 +202,32 @@ let menuIcon = document.getElementById('menuIcon');
 // Add click event listener to the search icon
 searchIcon.addEventListener('click', function() {
     // Toggle visibility of the search bar
-    event.stopPropagation();
+    event.stopPropagation(); // Prevent click from propagating to the document
 
+    // Check if the search bar is hidden or not
     if (searchBar.style.display === "none" || searchBar.style.display === "") {
+       // If hidden, show the search bar and hide the search and menu icons
         searchBar.style.display = "block";
         menuIcon.style.display = "none";
         searchIcon.style.display = "none";
         searchBar.focus(); // Automatically focus on the search bar
     } else {
+      // If visible, hide the search bar
         searchBar.style.display = "none";
     }
 });
 
+// Add click event listener to close the search bar when clicking outside of it
 document.addEventListener('click', function(event){
   if(!searchBar.contains(event.target) && event.target !==searchIcon){
+    // Hide the search bar and restore visibility of the icons
     searchBar.style.display = "none";
     menuIcon.style.display = "block";
     searchIcon.style.display = "block";
   }
 })
 
+// Get the body element
 let body = document.body;
 
 // When the user clicks the menu icon, toggle the visibility of the navbar
